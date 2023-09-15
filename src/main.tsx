01d7b9c64a74aa2a2f456b358d25a70901b63e9c
Alexandros Karagiannis <pp.alexkara@gmail.com>
2023-09-14 21:39:05 -0600

    feat(fake): fake

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import { App } from "src/App.tsx";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "src/theme/theme.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
