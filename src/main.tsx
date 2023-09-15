20f3198a1d613680dbf57024a6b2211220199cad
Alexandros Karagiannis <pp.alexkara@gmail.com>
2023-09-14 19:43:20 -0600

    feat(MUI): created theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from 'src/App.tsx'
+import { ThemeProvider } from '@mui/material'
+import { theme } from 'src/theme/theme.tsx'
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <CssBaseline />
+    <ThemeProvider theme={theme}>
+      
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
