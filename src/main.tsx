daa4f94838000b1281fdbdac1db01e49c5879f33
Alexandros Karagiannis <pp.alexkara@gmail.com>
2023-09-14 20:00:46 -0600

    feat(formatter): added prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from 'src/App.tsx'
-import { ThemeProvider } from '@mui/material'
-import { theme } from 'src/theme/theme.tsx'
-import CssBaseline from '@mui/material/CssBaseline';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "src/App.tsx";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "src/theme/theme.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-     <CssBaseline />
+    <CssBaseline />
     <ThemeProvider theme={theme}>
-      
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
